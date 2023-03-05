7798c290e737769f8bbf01ef12b8063cd3f16923
bazhil <i-bazh@mail.ru>
2023-03-05 21:49:43 +0300

    Add uploadfile

--- main.py
+++ main.py
@@ -118,14 +118,14 @@ def index():
     return {'Message': 'Hello!'}
 
 
-@app.post('uploadfile/profile')
+@app.post('/uploadfile/profile')
 async def create_upload_file(file: UploadFile=File(...), user: user_pydantic = Depends(get_current_user)):
     FILEPATH = './static/images/'
     filename = file.filename
-    extension = filename.split('. ')[1]
+    extension = filename.split('.')[1]
 
     if extension not in ['png', 'jpg']:
-        return {'ststus': 'error', 'detail': 'File extension not allowed'}
+        return {'status': 'error', 'detail': 'File extension not allowed'}
 
     token_name = secrets.token_hex(10) + '.' + extension
     generated_name = FILEPATH + token_name
@@ -135,7 +135,7 @@ async def create_upload_file(file: UploadFile=File(...), user: user_pydantic = D
         file.write(file_content)
 
     # PILLOW
-    img = Image.opem(generated_name)
+    img = Image.open(generated_name)
     img = img.resize(size=(200, 200))
     img.save(generated_name)
     
@@ -152,6 +152,47 @@ async def create_upload_file(file: UploadFile=File(...), user: user_pydantic = D
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail='Not authenticated to perform this action',
             headers={'WWW-Authenticate': 'Bearer'})
+
+    file_url = 'localhost:8000' + generated_name[1:]
+
+    return {'status': 'ok', 'filename': file_url}
+
+@app.post('/uploadfile/products/{id}')
+async def create_upload_file(id: int, file: UploadFile = File(...), user: user_pydantic = Depends(get_current_user)):
+    FILEPATH = './static/images/'
+    filename = file.filename
+    extension = filename.split('.')[1]
+
+    if extension not in ['png', 'jpg']:
+        return {'status': 'error', 'detail': 'File extension not allowed'}
+
+    token_name = secrets.token_hex(10) + '.' + extension
+    generated_name = FILEPATH + token_name
+    file_content = await file.read()
+
+    with open(generated_name, 'wb') as file:
+        file.write(file_content)
+
+    # PILLOW
+    img = Image.open(generated_name)
+    img = img.resize(size=(200, 200))
+    img.save(generated_name)
+
+    file.close()
+
+    product = await Product.get(id=id)
+    business = await product.business
+    owner = await business.owner
+
+    if owner == user:
+        product.product_image = token_name
+        await product.save()
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Not authenticated to perform this action',
+            headers={'WWW-Authenticate': 'Bearer'})
+
 
 register_tortoise(
     app,
